0528dd3c2590b8197f69ff5b78ff08297cbc8245
Christopher McArthur <christopherm@jfrog.com>
2022-12-05 14:13:06 -0800

    fix up

--- .conan/extensions/commands/cci/cmd_export_all_versions.py
+++ .conan/extensions/commands/cci/cmd_export_all_versions.py
@@ -2,6 +2,9 @@ import os
 import json
 import yaml
 
+# conan config install .conan
+# conan cci:export-all-versions fmt
+
 from conan.api.output import ConanOutput
 from conan.cli.command import conan_command
 
@@ -10,24 +13,22 @@ def output_json(exported):
 
 
 @conan_command(group="Conan Center Index", formatters={"json": output_json})
-def multi_export(conan_api, parser, *args):
+def export_all_versions(conan_api, parser, *args):
     """
     Exporting several recipes
     """
     parser.add_argument('name')
     args = parser.parse_args(*args)
     result = []
-        
-    knownfolders = {}
 
-    recipefolder = os.path.join(os.cwd, "recipes", args.name)
+    recipefolder = os.path.join("recipes", args.name)
     if not os.path.isdir(recipefolder):
       print("ABORTING -- Make sure to run from CCI root")
-      
+
     configfile = os.path.join(recipefolder, "config.yml")
     if os.path.isfile(configfile):
        with open(configfile, "r") as file:
-          config = yaml.load_safe(configfile)
+          config = yaml.safe_load(file)
           for version in config["versions"]:
             conanfile = os.path.join(recipefolder, config["versions"][version]["folder"], "conanfile.py")
             if os.path.isfile(conanfile):
