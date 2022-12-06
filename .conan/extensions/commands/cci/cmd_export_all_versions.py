4dbc2aca7905d8a34801a3eb119bcf441e4eadcf
Christopher McArthur <christopherm@jfrog.com>
2022-12-06 11:05:10 -0800

    out put and formatters

--- .conan/extensions/commands/cci/cmd_export_all_versions.py
+++ .conan/extensions/commands/cci/cmd_export_all_versions.py
@@ -9,7 +9,7 @@ from conan.api.output import ConanOutput
 from conan.cli.command import conan_command, OnceArgument
 
 def output_json(exported):
-    return json.dumps({"exported": [repr(r) for r in exported]})
+    print(json.dumps({"exported": [r for r in exported]}))
 
 
 @conan_command(group="Conan Center Index", formatters={"json": output_json})
@@ -20,14 +20,12 @@ def export_all_versions(conan_api, parser, *args):
     parser.add_argument('-n', '--name')
     parser.add_argument('-l', '--list', action=OnceArgument, help="YAML file with list of recipes to export")
     args = parser.parse_args(*args)
-    result = []
 
     out = ConanOutput()
 
     recipes_to_export = []
-
     if args.list:
-        out.writeln(f"Parsing recipes from list {args.list}")
+        out.verbose(f"Parsing recipes from list {args.list}")
         with open(args.list, "r") as stream:
             try:
                 recipes_to_export = yaml.safe_load(stream)['recipes']
@@ -40,19 +38,19 @@ def export_all_versions(conan_api, parser, *args):
     failed = set()
 
     for item in recipes_to_export:
-
         recipe_name = item if not isinstance(item, dict) else list(item.keys())[0]
         folders = None if not isinstance(item, dict) else item[recipe_name][0]['folders']
 
         recipe_folder = os.path.join("recipes", recipe_name)
-
         if not os.path.isdir(recipe_folder):
-            out.error("ABORTING - Path does not exist")
+            out.error(f"ABORTING - {recipe_name}'s folder does not exist")
+            return
 
         config_file = os.path.join(recipe_folder, "config.yml")
 
         if not os.path.exists(config_file):
             out.error(f"ABORTING: file {config_file} does not exist")
+            return
 
         with open(config_file, "r") as file:
             config = yaml.safe_load(file)
@@ -62,23 +60,22 @@ def export_all_versions(conan_api, parser, *args):
                     continue
 
                 conanfile = os.path.join(recipe_folder, recipe_subfolder, "conanfile.py")
-                out.writeln(f"Conanfile to export: {conanfile}")
+                out.verbose(f"Conanfile to export: {conanfile}")
                 if os.path.isfile(conanfile):
                     out.verbose(f"Exporting {recipe_name}/{version}")
                     try:
                         ref = conan_api.export.export(os.path.abspath(conanfile), recipe_name, version, None, None)
-                        out.writeln(f"Exported {ref}")
-                        result.append(ref)
+                        out.verbose(f"Exported {ref}")
                         exported.append(ref)
-                    except:
-                        failed.add(f"{recipe_name}/{recipe_subfolder}")
+                    except Exception as e:
+                        failed.add((ref, str(e)))
 
-    out.writeln("--------- EXPORTED RECIPES ---------")
+    out.title("EXPORTED RECIPES")
     for item in exported:
-        out.writeln(f"{item[0]}")
+        out.info(f"{item[0]}")
 
-    out.writeln("--------- FAILED TO EXPORT  ---------")
+    out.title("FAILED TO EXPORT")
     for item in failed:
-        out.writeln(item)
+        out.info(f"{item[0]}, reason: {item[1]}")
 
-    return result
+    return exported
