11adb311cd70da801450fadb729402397fc84521
Christopher McArthur <christopherm@jfrog.com>
2022-12-06 14:01:44 -0800

    ux and cleanup
    removed logic of folder here since we always do all of them

--- .conan/extensions/commands/cci/cmd_export_all_versions.py
+++ .conan/extensions/commands/cci/cmd_export_all_versions.py
@@ -8,6 +8,7 @@ import yaml
 
 from conan.api.output import ConanOutput
 from conan.cli.command import conan_command, OnceArgument
+from conan.errors import ConanException
 
 def output_json(results):
     print(json.dumps({
@@ -52,13 +53,14 @@ def export_all_versions(conan_api, parser, *args):
     """
     Export all version for a recipe
     """
-    parser.add_argument('-n', '--name')
+    parser.add_argument('-n', '--name', action=OnceArgument, help="Name of the recipe to export")
     parser.add_argument('-l', '--list', action=OnceArgument, help="YAML file with list of recipes to export")
     args = parser.parse_args(*args)
 
     out = ConanOutput()
 
     recipes_to_export = []
+
     if args.list:
         out.verbose(f"Parsing recipes from list {args.list}")
         with open(args.list, "r") as stream:
@@ -66,44 +68,47 @@ def export_all_versions(conan_api, parser, *args):
                 recipes_to_export = yaml.safe_load(stream)['recipes']
             except yaml.YAMLError as exc:
                 print(exc)
+    elif args.name:
+        recipes_to_export = [args.name]
     else:
-        recipes_to_export = args.name
+        raise ConanException("Must specify at least -n or -l args for this to work")
 
+    assert isinstance(recipes_to_export, list), "The code expects this to be an array"
+
+    # Result output variables, these should always be returned
     exported = []
     failed = dict()
 
     for item in recipes_to_export:
         recipe_name = item if not isinstance(item, dict) else list(item.keys())[0]
-        folders = None if not isinstance(item, dict) else item[recipe_name][0]['folders']
+        out.verbose(f"Starting recipe '{recipe_name}'")
 
         recipe_folder = os.path.join("recipes", recipe_name)
         if not os.path.isdir(recipe_folder):
-            out.error(f"ABORTING - {recipe_name}'s folder does not exist")
+            out.error(f"Invalid user input: '{recipe_name}' folder does not exist")
             return exported, failed
 
         config_file = os.path.join(recipe_folder, "config.yml")
-
-        if not os.path.exists(config_file):
-            out.error(f"ABORTING: file {config_file} does not exist")
+        if not os.path.isfile(config_file):
+            out.error(f"The file {config_file} does not exist")
             return exported, failed
 
         with open(config_file, "r") as file:
             config = yaml.safe_load(file)
             for version in config["versions"]:
                 recipe_subfolder = config["versions"][version]["folder"]
-                if folders and not  recipe_subfolder in folders:
-                    continue
-
                 conanfile = os.path.join(recipe_folder, recipe_subfolder, "conanfile.py")
-                out.verbose(f"Conanfile to export: {conanfile}")
-                if os.path.isfile(conanfile):
-                    out.verbose(f"Exporting {recipe_name}/{version}")
-                    try:
-                        ref = conan_api.export.export(os.path.abspath(conanfile), recipe_name, version, None, None)
-                        out.verbose(f"Exported {ref}")
-                        exported.append(ref)
-                    except Exception as e:
-                        failed.update({f"{recipe_name}/{recipe_subfolder}": str(e)})
+                if not os.path.isfile(conanfile):
+                    out.error(f"The file {conanfile} does not exist")
+                    return exported, failed
+
+                out.verbose(f"Exporting {recipe_name}/{version} from {recipe_subfolder}/")
+                try:
+                    ref = conan_api.export.export(os.path.abspath(conanfile), recipe_name, version, None, None)
+                    out.verbose(f"Exported {ref}")
+                    exported.append(ref)
+                except Exception as e:
+                    failed.update({f"{recipe_name}/{recipe_subfolder}": str(e)})
 
     out.title("EXPORTED RECIPES")
     for item in exported:
