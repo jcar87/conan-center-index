7dcb03f298081abd108b855bc13cd9a61a804297
Christopher McArthur <christopherm@jfrog.com>
2022-12-06 11:44:18 -0800

    dont put yaml files in workflow folder
    github does not like this

--- .github/export-and-build-all.sh
+++ .github/export-and-build-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 conan remove "*" -c
-conan cci:export-all-versions --list=.github/workflows/top_100_recipes.yaml
-conan cci:create-top-versions --list=.github/workflows/top_100_recipes.yaml
+conan cci:export-all-versions --list=.github/top_100_recipes.yaml
+conan cci:create-top-versions --list=.github/top_100_recipes.yaml
